3a44329efda42d592ed72b2643dfa0ecb1697e23
Mene90 <menelaosralli@gmail.com>
2017-03-02 10:56:02 +0100

    Test7 ready to start

---
+++ Test/Test7/Test7.m
@@ -0,0 +1,86 @@
+% addpath('C:\Users\mene9\Documents\MATLAB\ESSFM_Sim\Test\Safe_Sim\')
+% addpath('/home/menelaos/MATLAB/ESSFM_Sim/Test/Safe_Sim/');
+
+symbols      = [2^14];
+
+forward_steps = [10, 10, 50, 50];
+Rs            = [10, 25,100,200];
+Fn            = [5];
+etasp         = [0.5 .*10.^(Fn/10)];
+Nspan         = 40;
+
+N_steps{1}  = [1,2,4,6,10];
+N_steps{2}  = [1,2,4,6,10];
+N_steps{3}  = [1,2,8,16,32,50];
+N_steps{4}  = [1,2,8,16,32,50];
+
+N_coefficients{1}  = [2,4];
+N_coefficients{2}  = [2,4];
+N_coefficients{3}  = [2,4,8];
+N_coefficients{4}  = [2,4,8,16];
+
+for k = 1:length(Rs)
+    
+    n_prop_steps = forward_steps(k);
+    symbrate     = Rs(k);
+    NS           = N_steps{k};
+    Nc           = N_coefficients{k};
+    
+    parfor  i = 1:length(NS)
+        ssfm_max_snr(i) = SSFM_MAX_SNR(NS(i),symbols,n_prop_steps,symbrate,etasp,Nspan);
+        
+    end
+    print = ['SSFM NS = [',int2str(NS),'] Max SNR = [',int2str(ssfm_max_snr),'] dB '];
+    disp(print);
+    
+    
+    tic
+    for  j = 1:length(Nc)
+        nc = Nc(j);
+        parfor  i = 1:length(NS)
+            max_snr(i,j) = ESSFM_MAX_SNR(NS(i),nc,symbols,n_prop_steps,symbrate,etasp,Nspan);
+        end
+        print = ['ESSFM NS = [',int2str(NS),'] Max SNR = [',int2str(max_snr(:,j)'),'] dB ','NC = ',int2str(nc)];
+        disp(print);
+    end
+    toc
+    
+    fig = figure(j);
+    
+    lgn = [];
+    
+    colors  = {'-ob';'-og';'-or';'-oc'...
+               '-+b';'-+g';'-+r';...
+               '-*b';'-*g';'-*r'};
+    
+    lgn     = ({['SSFM']});
+    for i = 1:length(Nc)
+        tmp(i,:)  = ({['ESSFM Nc = ' int2str(Nc(i))]});
+        lgn       = [lgn tmp(i,:)];
+    end
+    
+    p1 = plot(NS, ssfm_max_snr,'-*k');
+    hold('on')
+    for i= 1:length(Nc)
+        p1=plot(NS, max_snr(:,i), colors{i});
+        hold('on')
+    end
+    t = strcat('SSFM vs ESSFM N_{step} =',{' '},int2str(n_prop_steps),{' '},...
+        'F_n = '    ,{' '}, int2str(Fn)    ,'dB',{' '},...
+        'N_{Span}= ',{' '}, int2str(Nspan) ,     {' '},...
+        'R = '      ,{' '}, int2str(symbrate),   {' '},...
+        'N_{symbols}',{' '}, '2^{', int2str(log2(symbols)) ,{'}'});
+    
+    title(t);
+    
+    grid on;
+    ylabel('SNR [dB]');
+    xlabel('n° steps');
+    
+    legend(lgn)
+    
+    savefig(fig,strcat('plot/S_vs_E_', int2str(n_prop_steps),'_',int2str(symbrate),'_',int2str(log2(symbols)),'.fig'))
+    
+    hold('off')
+    close(fig);
+end
---
+++ Test/Test7/ssfm_maxSNR.m
@@ -0,0 +1,51 @@
+function [ max_snr ] = ssfm_maxSNR(ch,dsp,sig,etasp,Nspan)
+%MINBER Summary of this function goes here
+%   Detailed explanation goes here
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                         Amplifier parameters                           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Gerbio    = ch.alphalin/(log(10)*1e-4)*ch.Lf*1e-3;
+ampli.G   = Gerbio;
+ampli.e   = etasp;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                         Some System Parameters                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nfft      = sig.NSYMB*sig.NT;
+Loss      = 10^(-Gerbio*0.1);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                         Pulse parameters                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+pls.shape   = 'RRC';
+pls.bw      = 1.0;                       % duty cycle
+pls.ord     = 0.1;                       % pulse roll-off
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%              Modulation of the Signal used for SNR  calc               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                       SNR Matched filter                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Hf_SNR        = transpose(filt(pls,sig.FN));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[patx_tx(:,1), patmatx_tx]    = Pattern.random(4,sig.NSYMB);
+[paty_tx(:,1), patmaty_tx]    = Pattern.random(4,sig.NSYMB);
+
+E                             = Laser.GetLaserSource(1, nfft);
+
+set(sig,'FIELDX'    ,Modulator.ApplyModulation(E, 2*patmatx_tx-1, sig, pls));
+set(sig,'FIELDX_TX' ,Modulator.ApplyModulation(E, 2*patmatx_tx-1, sig, pls));
+set(sig,'FIELDY'    ,Modulator.ApplyModulation(E, 2*patmaty_tx-1, sig, pls));
+set(sig,'FIELDY_TX' ,Modulator.ApplyModulation(E, 2*patmaty_tx-1, sig, pls));
+
+system.Nspan     = Nspan;
+system.Loss      = Loss;
+system.mfil_snr  = Hf_SNR;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+options = optimset('Display','off','TolX',3e-1);
+funmax = @(Ps_dBm) -par_ssfm_snr(Ps_dBm,ch,dsp,sig,ampli,system);
+
+[P_max,max_snr] = fminbnd(funmax,-2,10,options);
+end
