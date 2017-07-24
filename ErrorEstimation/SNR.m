6e2e3246517d67d21021a57c308fcf81f99a8c66
Mene <menelaosralli@gmail.com>
2017-07-24 18:03:18 +0200

    backup

--- ErrorEstimation/SNR.m
+++
@@ -1,6 +0,0 @@
-function [ snr ] = SNR( patmat_tx,patmat_rx )
-
-    snr = var(patmat_tx)/var(patmat_tx - patmat_rx);
-
-end
-
--- ErrorEstimation/ber.m
+++
@@ -1,8 +0,0 @@
-function [ avg_ber ] = ber( patmat_hat,patmat_rx )
-   
-
-    avg_ber = 0.5*(mean(patmat_hat(:,1)~=patmat_rx(:,1))+ mean(patmat_hat(:,2)~=patmat_rx(:,2)));
-      
-
-end
-
